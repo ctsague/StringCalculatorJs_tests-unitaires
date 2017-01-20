38783bfd0538e83f7bc5b231e39f51e5c5767328
tsague <tsagueymele@yahoo.fr>
2017-01-20 14:24:35 +0100

    test return 0 when ''

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -5,5 +5,8 @@ describe("StringCalculator execution", function () {
     it("should return 0 when input is null", function () {
         expect(StringCalculator(null)).toBe(0);
     });
+     it("should return 0 when input is '' ", function () {
+        expect(StringCalculator("")).toBe(0);
+    });
 
 });

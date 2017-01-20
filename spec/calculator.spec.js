778860407bb7ee83f58ab00654dffe0201a0f6d7
tsague <tsagueymele@yahoo.fr>
2017-01-20 14:32:13 +0100

    test return 1 when input is '1'

--- spec/calculator.spec.js
+++ spec/calculator.spec.js
@@ -9,7 +9,9 @@ describe("StringCalculator execution", function () {
         expect(StringCalculator("")).toBe(0);
     });
      it("should return 0 when input is '0' ", function () {
-        expect(StringCalculator("")).toBe(0);
+        expect(StringCalculator('0')).toBe(0);
     });
-
+     it("should return 1 when input is '1' ", function () {
+        expect(StringCalculator("1")).toBe(1);
+    });
 });
